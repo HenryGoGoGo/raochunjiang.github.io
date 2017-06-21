e780f2582177675dff56161645eb9419cfaf481a
raochunjiang <287573755@qq.com>
2017-06-21 16:26:29 +0800

    添加 sass 编译任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,30 @@
-var gulp = require('gulp');
-gulp.task('default',function(){
-    
-})
+(function (r) {
+    'use strict';
+
+    var gulp = r('gulp');
+    var sass = r('gulp-sass');
+
+    gulp.task('sass:build', function () {
+        return gulp.src('styles/scss/**/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('styles/css'));
+    });
+
+
+    // 生成
+    gulp.task('build', ['sass:build'], function () {
+
+    });
+    // 发布
+    gulp.task('release', []);
+
+    gulp.task('default', ['build']);
+
+    /**
+     * 实时监听
+     */
+    gulp.task('sass:watch', function () {
+        gulp.watch('./sass/**/*.scss', ['sass']);
+    });
+
+})(require)
